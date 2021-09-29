13a6a8ad7dd3fa372ca031e8cf997129502b79c6
Andrey Beloboky <andrei.beloboky@yandex.ru>
2021-09-30 00:13:20 +0300

    add create number cats dogs and cows

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number cat(s)");
+        int catNumber = scanner.nextInt();
+        System.out.println("Enter number dog(s)");
+        int dogNumber = scanner.nextInt();
+        System.out.println("Enter number caw(s)");
+        int cowNumber = scanner.nextInt();
     }
 }
